540174782845ac0383166eabd57c21970f9edf84
mrr79 <marianarojas2197@gmail.com>
2024-03-22 13:30:12 -0600

    estado del pedido listo

--- App1/App1/FacturaPage.xaml.cs
+++ App1/App1/FacturaPage.xaml.cs
@@ -6,11 +6,14 @@ namespace App1
 {
     public partial class FacturaPage : ContentPage
     {
-        // Variable estática para mantener el número consecutivo
         private static int numeroConsecutivo = 1;
 
         // Propiedad para vincular el número de factura con el Label en el XAML
         public int NumeroFactura { get; set; }
+
+        private ObservableCollection<ItemMenu> carrito;
+
+        // Constructor y otros miembros de la clase
 
         public FacturaPage(ObservableCollection<ItemMenu> carrito, double total)
         {
@@ -37,6 +40,20 @@ namespace App1
 
             // Establecer el contexto de datos de la página como esta instancia de FacturaPage
             BindingContext = this;
+
+            // Guardar el carrito en un campo de la clase
+            this.carrito = carrito;
+        }
+
+        // Método para manejar el evento de visualizar estado del pedido
+        async void OnVisualizarEstadoPedidoClicked(object sender, EventArgs e)
+        {
+            // Aquí colocas la lógica para abrir la página de visualización del estado del pedido
+            // Por ejemplo:
+            EstadoPage estadoPage = new EstadoPage(this.carrito);
+            await Navigation.PushAsync(estadoPage);
         }
     }
+
+
 }
